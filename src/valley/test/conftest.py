96b89bd17607800672eea7469bd74a2958815492
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2020-06-28 15:58:48 +0900

    added test code

--- src/valley/test/conftest.py
+++ src/valley/test/conftest.py
@@ -1,15 +1,13 @@
-"""
-test with pytest fixture
-"""
-
 import pytest
 
 
 @pytest.fixture
 def csv_file():
-    # return 'csv file!!!'
-    yield 'csv file!!!'
+    return "csv file!!!"
 
 
 def pytest_addoption(parser):
-    parser.addoption('--os-name', default='linux', help='os name')
+    """
+    test with pytest fixture
+    """
+    parser.addoption('--os-name', default='linux', help='OS Name')
--- src/valley/test/test_with_fixture.py
+++ src/valley/test/test_with_fixture.py
@@ -2,6 +2,7 @@ import pytest
 import calculation
 import os
 
+
 class TestCalTmpdir(object):
 
     @classmethod
@@ -13,12 +14,21 @@ class TestCalTmpdir(object):
     def setup_method(self, method):
         print(f'method={method}')
 
-    def test_add_num_and_double(self):
+    def test_add_num_and_double(self, request):
+        os_name = request.config.getoption('--os-name')
+        if os_name == 'mac' or os_name == 'linux':
+            print('ls')
+        elif os_name == 'windows':
+            print('dir')
         assert self.cal.add_num_and_double(1, 1) == 4
 
-    def test_save(self, tmpdir, csv_file):
+    def test_save(self, tmpdir):
         # 'tmpdir'は'pytest'のfixture
+        print(tmpdir)
         self.cal.save(tmpdir, self.test_file_name)
-        # csv_fileは'conftest'のfixture
         test_file_path = os.path.join(tmpdir, self.test_file_name)
         assert os.path.exists(test_file_path)
+
+    def test_csv_file(self, csv_file):
+        print(csv_file)
+        assert csv_file == 'csv file!!!'
