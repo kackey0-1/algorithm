3dc3708eb97a2f10fe48785d8e832d82b06a464e
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2020-07-12 23:42:20 +0900

    added cypto

---
+++ src/valley/crypt/crypto_ctrl.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+from Crypto.Cipher import AES
+
+print(AES.block_size)
+print(string.ascii_letters)
+key = ''.join(
+    random.choice(string.ascii_letters) for _ in range(AES.block_size))
+
+iv = ''.join(
+    random.choice(string.ascii_letters) for _ in range(AES.block_size))
+
+print(key, iv)
+
