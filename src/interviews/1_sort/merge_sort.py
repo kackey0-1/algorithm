d9617fe46bef5ae9c8c9920371d17074cbc40aef
k-kackey0-1 <t-kentaro.kakimoto@paypay-corp.co.jp>
2020-11-14 19:07:37 +0900

    adding select, insert

---
+++ src/interviews/1_sort/merge_sort.py
@@ -0,0 +1,24 @@
+from typing import List
+import random
+
+
+def partition() -> int:
+    return
+
+
+def quick(nums: List) -> List:
+    def _quick_sort(nums: List, low: int, high: int):
+        pass
+
+
+if __name__ == '__main__':
+    numbers = [random.randint(0, 100) for i in range(10)]
+    print(numbers)
+
+
+
+
+
+
+
+
---
+++ src/interviews/1_sort/quick_sort.py
@@ -0,0 +1,12 @@
+from typing import List
+import random
+
+
+def quick(nums: List) -> List:
+    return nums
+
+
+if __name__ == '__main__':
+    arr = [random.randint(0, 100) for i in range(10)]
+    print(arr)
+    print(quick(arr))
