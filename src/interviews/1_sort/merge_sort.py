a21bdcb1eaeac46df40040e62a23a92511e63de5
k-kackey0-1 <t-kentaro.kakimoto@paypay-corp.co.jp>
2020-11-24 09:37:05 +0900

    adding merge/quick/binary

--- src/interviews/1_sort/merge_sort.py
+++ src/interviews/1_sort/merge_sort.py
@@ -2,18 +2,44 @@ from typing import List
 import random
 
 
-def partition() -> int:
-    return
+def merge(nums: List) -> List:
+    if len(nums) <= 1:
+        return nums
 
+    center = len(nums) // 2
+    left = nums[:center]
+    right = nums[center:]
 
-def quick(nums: List) -> List:
-    def _quick_sort(nums: List, low: int, high: int):
-        pass
+    merge(left)
+    merge(right)
+
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            nums[k] = left[i]
+            i += 1
+        else:
+            nums[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        nums[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        nums[k] = right[j]
+        j += 1
+        k += 1
+
+    return nums
 
 
 if __name__ == '__main__':
     numbers = [random.randint(0, 100) for i in range(10)]
     print(numbers)
+    print(merge(numbers))
 
 
 
--- src/interviews/1_sort/quick_sort.py
+++ src/interviews/1_sort/quick_sort.py
@@ -2,11 +2,35 @@ from typing import List
 import random
 
 
+def partition(numbers: List[int], low: int, high: int) -> int:
+    i = low - 1
+    pivot = numbers[high]
+    for j in range(low, high):
+        if numbers[j] < pivot:
+            i += 1
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+
+    return i+1
+
+
 def quick(nums: List) -> List:
+    def _quick_sort(numbers: List, low: int, high: int):
+        partition_index = partition(numbers, low, high)
+        _quick_sort(numbers, partition_index+1, high)
+        _quick_sort(numbers, low, partition_index-1)
+    _quick_sort(nums, 0, len(nums)-1)
     return nums
 
 
 if __name__ == '__main__':
-    arr = [random.randint(0, 100) for i in range(10)]
-    print(arr)
-    print(quick(arr))
+    numbers = [random.randint(0, 100) for i in range(10)]
+    print(numbers)
+    print(quick(numbers))
+
+
+
+
+
+
+
+
