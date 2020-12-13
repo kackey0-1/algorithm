1094f3eead93bd7c58f5f6a27293c2a9a7ae708a
k-kackey0-1 <t-kentaro.kakimoto@paypay-corp.co.jp>
2020-12-13 19:53:42 +0900

    adding summary sorts

--- src/interviews/1_sort/summary_sorts.py
+++ src/interviews/1_sort/summary_sorts.py
@@ -61,16 +61,34 @@ def merge_sort(numbers: List[int]):
         k += 1
 
 
-def quick_sort(numbers: List[int]):
-    def _sort(numbers: List[int], low: int, high: int):
+# def quick_sort(numbers: List[int]):
+#     def _sort(numbers: List[int], low: int, high: int):
+#         if low < high:
+#             partition_index = partition(numbers, low, high)
+#             _sort(numbers, partition_index, high)
+#             _sort(numbers, low, partition_index - 1)
+#     _sort(numbers, 0, len(numbers)-1)
+#
+#
+# def partition(numbers: List[int], low: int, high: int):
+#     i = low - 1
+#     pivot = numbers[high]
+#     for j in range(low, high):
+#         if numbers[j] <= pivot:
+#             i += 1
+#             numbers[i], numbers[j] = numbers[j], numbers[i]
+#     numbers[i+1], numbers[high] = numbers[high], numbers[i+1]
+#     return i+1
+def quick_sort(numbers: List[int]) -> None:
+    def _sort(numbers: List[int], low: int, high: int) -> None:
         if low < high:
             partition_index = partition(numbers, low, high)
-            _sort(numbers, partition_index, high)
             _sort(numbers, low, partition_index - 1)
+            _sort(numbers, partition_index, high)
     _sort(numbers, 0, len(numbers)-1)
 
 
-def partition(numbers: List[int], low: int, high: int):
+def partition(numbers: List[int], low: int, high: int) -> int:
     i = low - 1
     pivot = numbers[high]
     for j in range(low, high):
@@ -82,25 +100,25 @@ def partition(numbers: List[int], low: int, high: int):
 
 
 if __name__ == '__main__':
-    numbers = [random.randint(0, 100) for i in range(10)]
-    print(numbers)
-    bubble_sort(numbers)
-    print(numbers)
-
-    numbers = [random.randint(0, 100) for i in range(10)]
-    print(numbers)
-    select_sort(numbers)
-    print(numbers)
-
-    numbers = [random.randint(0, 100) for i in range(10)]
-    print(numbers)
-    insert_sort(numbers)
-    print(numbers)
-
-    numbers = [random.randint(0, 100) for i in range(10)]
-    print(numbers)
-    merge_sort(numbers)
-    print(numbers)
+    # numbers = [random.randint(0, 100) for i in range(10)]
+    # print(numbers)
+    # bubble_sort(numbers)
+    # print(numbers)
+    #
+    # numbers = [random.randint(0, 100) for i in range(10)]
+    # print(numbers)
+    # select_sort(numbers)
+    # print(numbers)
+    #
+    # numbers = [random.randint(0, 100) for i in range(10)]
+    # print(numbers)
+    # insert_sort(numbers)
+    # print(numbers)
+    #
+    # numbers = [random.randint(0, 100) for i in range(10)]
+    # print(numbers)
+    # merge_sort(numbers)
+    # print(numbers)
 
     numbers = [random.randint(0, 100) for i in range(10)]
     print(numbers)
