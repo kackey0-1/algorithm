0b7f838fa567d5b7dccc95dd48b6b9b368dad1ad
n-nishizuka <n-nishizuka@rough-and-ready.co.jp>
2020-12-22 09:55:33 +0900

    adding doubly linked list summary.py

--- src/interviews/3_linked_list/summary.py
+++ src/interviews/3_linked_list/summary.py
@@ -3,15 +3,16 @@ from typing import Any
 
 class Node(object):
 
-    def __init__(self, data: Any):
+    def __init__(self, data: Any, next=None, prev=None):
         self.data = data
-        self.next = None
+        self.next = next
+        self.prev = prev
 
 
 class LinkedList(object):
 
-    def __init__(self):
-        self.head = None
+    def __init__(self, head: Node = None):
+        self.head = head
 
     def append(self, data: Any) -> None:
         new_node = Node(data)
@@ -70,19 +71,53 @@ class LinkedList(object):
         _reverse(self.head, None)
 
 
+class DoublyLinkedList(object):
+
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, data: Any) -> None:
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+
+        current_node.next = new_node
+        new_node.prev = current_node
+
+
 if __name__ == '__main__':
 
-    l = LinkedList()
+    # l = LinkedList()
     # l.insert(0)
     # l.append(2)
     # l.append(4)
     # l.insert(19)
-    l.append(1)
-    l.append(2)
-    l.append(3)
-    l.append(4)
-    l.reverse_recursive()
-    l.print()
+    # l.append(1)
+    # l.append(2)
+    # l.append(3)
+    # l.append(4)
+    # l.reverse_recursive()
+    # l.print()
+
+    d = DoublyLinkedList()
+    d.append(0)
+    d.append(1)
+    d.append(2)
+    print(d.head.data)
+    print(d.head.next.data)
+    print(d.head.next.next.data)
+    print(d.head.next.next.prev.data)
+    print(d.head.next.next.prev.prev.data)
+
+
+
+
+
 
 
 
