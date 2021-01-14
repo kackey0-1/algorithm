baad99744f4d9e00af67213f271a4a349cc1c668
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2021-01-14 20:27:10 +0900

    adding bit fix

--- src/valley/http/xml_ctrl.py
+++ src/valley/http/xml_ctrl.py
@@ -1,28 +1,30 @@
 import xml.etree.ElementTree as ET
 
-root = ET.Element('root')
-tree = ET.ElementTree(element=root)
 
-employee = ET.SubElement(root, 'employee')
+if __name__ == '__main__':
+    # root = ET.Element('root')
+    # tree = ET.ElementTree(element=root)
+    #
+    # employee = ET.SubElement(root, 'employee')
+    #
+    # employ = ET.SubElement(employee, 'employ')
+    # employ_id = ET.SubElement(employ, 'id')
+    # employ_id.text = '111'
+    # employ_name = ET.SubElement(employ, 'name')
+    # employ_name.text = 'Mike'
+    #
+    # employ = ET.SubElement(employee, 'employ')
+    # employ_id = ET.SubElement(employ, 'id')
+    # employ_id.text = '222'
+    # employ_name = ET.SubElement(employ, 'name')
+    # employ_name.text = 'Ken'
+    #
+    # tree.write('data/tests.xml', encoding='utf-8', xml_declaration=True)
 
-employ = ET.SubElement(employee, 'employ')
-employ_id = ET.SubElement(employ, 'id')
-employ_id.text = '111'
-employ_name = ET.SubElement(employ, 'name')
-employ_name.text = 'Mike'
+    tree = ET.ElementTree(file='data/test.xml')
+    root = tree.getroot()
 
-employ = ET.SubElement(employee, 'employ')
-employ_id = ET.SubElement(employ, 'id')
-employ_id.text = '222'
-employ_name = ET.SubElement(employ, 'name')
-employ_name.text = 'Ken'
-
-tree.write('data/tests.xml', encoding='utf-8', xml_declaration=True)
-
-tree = ET.ElementTree(file='data/test.xml')
-root = tree.getroot()
-
-for employee in root:
-    for employ in employee:
-        for person in employ:
-            print(person.tag, person.text)
+    for employee in root:
+        for employ in employee:
+            for person in employ:
+                print(person.tag, person.text)
