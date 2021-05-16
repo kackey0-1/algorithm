d011952e8f5ed9adcc91fe78f57d0dad3f3f35ac
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2021-05-16 23:42:43 +0900

    add map.

--- src/drfapp/frontend/src/common/request.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const service = axios.create({
-  baseURL: '127.0.0.1:8000',
-  timeout: 300000,
-})
-
-export default service;
--- src/interviews/readme.md
+++
@@ -1,3 +0,0 @@
-# Understanding Algorithm by HandsOn
-
-[Algorithm 図解](https://docs.google.com/presentation/d/1l5KgvETEO28_o2ujzzYDQZRCando8K83A9tq38tdH3Q)
